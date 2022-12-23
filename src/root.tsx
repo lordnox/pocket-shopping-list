b8e77dff4a07ad240dc81b61d78d13fb35270470
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-23 02:51:51 +0100

    added favicon

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,19 @@
 // @refresh reload
 import { Suspense } from 'solid-js'
-import { useLocation, A, Body, ErrorBoundary, FileRoutes, Head, Html, Meta, Routes, Scripts, Title } from 'solid-start'
+import {
+  useLocation,
+  A,
+  Body,
+  ErrorBoundary,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Routes,
+  Scripts,
+  Title,
+  Link,
+} from 'solid-start'
 import './root.css'
 
 export default function Root() {
@@ -11,9 +24,13 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        <Title>Home Server</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <Link rel="manifest" href="/site.webmanifest" />
       </Head>
       <Body>
         <Suspense>
