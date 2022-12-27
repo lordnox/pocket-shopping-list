b6205578a685ed0194b1b3f3ab1eda309dc2262e
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-27 20:58:20 +0100

    updated client schema to allow VERCEL_URL

--- src/env/client.ts
+++ src/env/client.ts
@@ -4,7 +4,6 @@ const env = clientScheme.safeParse(import.meta.env)
 
 if (env.success === false) {
   console.error('❌ Invalid client environment variables:\n', ...formatErrors(env.error.format()))
-  console.log(import.meta.env)
   throw new Error('Invalid environment variables')
 }
 
--- src/env/schema.ts
+++ src/env/schema.ts
@@ -28,5 +28,11 @@ export const serverScheme = z.object({
 
 export const clientScheme = z.object({
   MODE: z.enum(['development', 'production', 'test']).default('development'),
-  START_BASE_URL: z.string(),
+  START_BASE_URL: z.preprocess(
+    // This makes Vercel deployments not fail if you don't set START_BASE_URL
+    // Since NextAuth.js automatically uses the VERCEL_URL if present.
+    (str) => process.env.VERCEL_URL ?? str,
+    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+    process.env.VERCEL ? z.string() : z.string().url(),
+  ),
 })
