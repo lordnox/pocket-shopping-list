5e7f0c0557acb9777a048a0a62289d3fb3bbd59a
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-27 20:52:48 +0100

    ...

--- src/env/server.ts
+++ src/env/server.ts
@@ -4,7 +4,6 @@ const env = serverScheme.safeParse(process.env)
 
 if (env.success === false) {
   console.error('❌ Invalid server environment variables:\n', ...formatErrors(env.error.format()))
-  console.log(env, process.env.VERCEL_URL, process.env.VERCEL)
   throw new Error('Invalid environment variables')
 }
 
