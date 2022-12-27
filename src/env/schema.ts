723519905e51b64ea4a1df022ffac4c2c8222efe
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-27 22:54:28 +0100

    synced VERCEL_URL handling server & client

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -1,3 +1,4 @@
+import { isServer } from 'solid-js/web'
 import { z, type ZodFormattedError } from 'zod'
 
 export const formatErrors = (errors: ZodFormattedError<Map<string, string>, string>) =>
@@ -7,6 +8,16 @@ export const formatErrors = (errors: ZodFormattedError<Map<string, string>, stri
     })
     .filter(Boolean)
 
+const START_BASE_URL = z.preprocess(
+  // This makes Vercel deployments not fail if you don't set START_BASE_URL
+  // Since NextAuth.js automatically uses the VERCEL_URL if present.
+  (str) => {
+    return (isServer ? process.env : import.meta.env).VERCEL_URL ?? str
+  },
+  // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+  (isServer ? process.env : import.meta.env).VERCEL ? z.string() : z.string().url(),
+)
+
 export const serverScheme = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   AUTH_TRUST_HOST: z
@@ -15,27 +26,12 @@ export const serverScheme = z.object({
     .transform((value) => value === 'true'),
   AUTH_SECRET: z.string(),
   DATABASE_URL: z.string(),
-  START_BASE_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set START_BASE_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => process.env.VERCEL_URL ?? str,
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
+  START_BASE_URL,
   GITHUB_CLIENT_ID: z.string(),
   GITHUB_CLIENT_SECRET: z.string(),
 })
 
 export const clientScheme = z.object({
   MODE: z.enum(['development', 'production', 'test']).default('development'),
-  START_BASE_URL: z.preprocess(
-    // This makes Vercel deployments not fail if you don't set START_BASE_URL
-    // Since NextAuth.js automatically uses the VERCEL_URL if present.
-    (str) => {
-      console.log(process?.env ?? import.meta.env)
-      return (process?.env ?? import.meta.env).VERCEL_URL ?? str
-    },
-    // VERCEL_URL doesn't include `https` so it cant be validated as a URL
-    process.env.VERCEL ? z.string() : z.string().url(),
-  ),
+  START_BASE_URL,
 })
