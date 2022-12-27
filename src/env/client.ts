7328a582da335b676057ae08c4134882d9edd198
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-27 21:06:55 +0100

    debug

--- src/env/client.ts
+++ src/env/client.ts
@@ -4,6 +4,7 @@ const env = clientScheme.safeParse(import.meta.env)
 
 if (env.success === false) {
   console.error('❌ Invalid client environment variables:\n', ...formatErrors(env.error.format()))
+  console.log(import.meta.env)
   throw new Error('Invalid environment variables')
 }
 
