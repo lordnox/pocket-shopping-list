4d759b422f78a0996c1a693cf831e98eea7be65b
Tobias Kopelke <tobias.kopelke@riskident.com>
2022-12-29 22:34:18 +0100

    added css group to have more content

--- src/components/shopping/item.tsx
+++ src/components/shopping/item.tsx
@@ -31,12 +31,12 @@ const rowClass = `
   grid
   grid-cols-[1fr_150px_150px]
   w-full
-  first:rounded-t-lg
-  last:rounded-b-lg
+  group-last:rounded-b-lg
 `
 
 const HeaderRow = createDiv(`
   ${rowClass}
+  rounded-t-lg
   text-xs
   text-gray-600
   uppercase
@@ -48,33 +48,35 @@ const HeaderRow = createDiv(`
 const ItemRow = createDiv(`
   ${rowClass}
   border-b
-  even:bg-gray-100
-  even:dark:bg-gray-700
-  odd:dark:bg-gray-800
+  group-even:bg-gray-100
+  group-even:dark:bg-gray-700
+  group-odd:dark:bg-gray-800
   dark:border-gray-700
-  hover:bg-gray-50
-  dark:hover:bg-gray-600
+  group-hover:bg-gray-50
+  group-hover:dark:bg-gray-600
 `)
 
 export const ShoppingItem: Component<{ item: ShoppingItemType }> = (props) => {
   return (
-    <ItemRow>
-      <th
-        scope="row"
-        classList={{
-          ['py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white']: true,
-          italic: props.item.optimistic,
-        }}
-      >
-        {props.item.name}
-      </th>
-      <td class="py-4 px-6">{(props.item.prices[0].normalizedPrice / 100).toFixed(2)} €</td>
-      <td class="py-4 px-6">
-        <PriceEntry>
-          <span>{(props.item.prices[0].price / 100).toFixed(2)} €</span>
-          <span> @ {amountString(props.item.prices[0].amount, props.item.type)}</span>
-        </PriceEntry>
-      </td>
-    </ItemRow>
+    <div class="group">
+      <ItemRow>
+        <th
+          scope="row"
+          classList={{
+            ['py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white']: true,
+            italic: props.item.optimistic,
+          }}
+        >
+          {props.item.name}
+        </th>
+        <td class="py-4 px-6">{(props.item.prices[0].normalizedPrice / 100).toFixed(2)} €</td>
+        <td class="py-4 px-6">
+          <PriceEntry>
+            <span>{(props.item.prices[0].price / 100).toFixed(2)} €</span>
+            <span> @ {amountString(props.item.prices[0].amount, props.item.type)}</span>
+          </PriceEntry>
+        </td>
+      </ItemRow>
+    </div>
   )
 }
